96e572545d8b0869d1568053684b0f105506d1a0
carlosherrerah <carlos.herrera@upa.edu.mx>
2026-01-13 11:13:37 -0600

    Auto 2026-01-13 11:13

--- subir.bat
+++ subir.bat
@@ -8,7 +8,7 @@ for /f %%i in ('wmic os get localdatetime ^| find "."') do set datetime=%%i
 set FECHA=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2% %datetime:~8,2%:%datetime:~10,2%
 
 git add .
-git commit -m "Automatico %FECHA%"
+git commit -m "Auto %FECHA%"
 git push
 
 pause
