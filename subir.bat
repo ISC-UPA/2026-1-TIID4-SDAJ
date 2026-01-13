beb82933d0b9ff4539b17cf27874b25fabe33d70
carlosherrerah <carlos.herrera@upa.edu.mx>
2026-01-13 11:05:24 -0600

    Automatico 2026-01-13 11:05

--- subir.bat
+++ subir.bat
@@ -1,10 +1,16 @@
+@echo off
 cls
 git pull
 git status
 timeout /t 2
+
+for /f %%i in ('wmic os get localdatetime ^| find "."') do set datetime=%%i
+set FECHA=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2% %datetime:~8,2%:%datetime:~10,2%
+
 git add .
-git commit -m "Automatico"
+git commit -m "Automatico %FECHA%"
 git push
+
 pause
 cls
 
